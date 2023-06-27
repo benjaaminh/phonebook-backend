eed3801e86c02d064bfd79faabe15efd397bed63
benja <benjamin.heikkala@abo.fi>
2023-06-27 19:39:17 +0300

    finish 3.6

--- index.js
+++ index.js
@@ -28,6 +28,59 @@ let persons=
       "number": "39-23-6423122"
     }
 ]
+const generateId = () => {
+  const id = Math.floor(Math.random()*100)
+  return id
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }else if(!body.number){
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  if (persons.map(person=>person.name).includes(person.name)){
+    return response.status(400).json({
+      error:'name must be unique'
+    })
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {  
+     response.json(person)  }
+      else {    
+        response.status(404).end()  }
+
+})
 
 app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p>
